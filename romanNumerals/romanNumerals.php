fa510911c20042e71fab24453e2eb061e55989e6
Ross Eastman <ross.eastman@mayden.co.uk>
2015-12-31 08:53:21 +0000

    done the converter, used lookup table, redefined funciton, tested edge

--- romanNumerals/romanNumerals.php
+++ romanNumerals/romanNumerals.php
@@ -3,10 +3,40 @@ namespace romanNumeralsPHP;
 
 class romanNumerals
 {
+    /**
+     * @var array
+     */
+    protected static $lookup = [
+        1000=> 'M',
+        900=> 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I'
+    ];
+
+    /**
+     * @param $number
+     * @return string
+     */
     public function convert($number)
     {
-        if ($number == 1) {
-            return 'I';
+        $solution = '';
+        foreach (static::$lookup as $limit => $glyph) {
+            while ($number >= $limit) {
+                $solution .= $glyph;
+                $number -= $limit;
+            }
+
         }
+
+        return $solution;
     }
 }
--- tests/unit/romanNumeralsTest.php
+++ tests/unit/romanNumeralsTest.php
@@ -15,4 +15,95 @@ class romanNumeralsTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('I', $testData);
     }
 
+    public function testItGivesIIfor2()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(2);
+        $this->assertEquals('II', $testData);
+    }
+
+    public function testItGivesIVfor4()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(4);
+        $this->assertEquals('IV', $testData);
+    }
+
+    public function testItGivesVfor5()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(5);
+        $this->assertEquals('V', $testData);
+    }
+
+    public function testItGivesVIfor6()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(6);
+        $this->assertEquals('VI', $testData);
+    }
+
+    public function testItGivesIVfor9()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(9);
+        $this->assertEquals('IX', $testData);
+    }
+
+    public function testItGivesXfor10()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(10);
+        $this->assertEquals('X', $testData);
+    }
+
+    public function testItGivesLfor50()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(50);
+        $this->assertEquals('L', $testData);
+    }
+
+    public function testItGivesXXfor20()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(20);
+        $this->assertEquals('XX', $testData);
+    }
+
+    public function testItGivesCfor100()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(100);
+        $this->assertEquals('C', $testData);
+    }
+
+    public function testItGivesDfor500()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(500);
+        $this->assertEquals('D', $testData);
+    }
+
+    public function testItGivesMfor1000()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(1000);
+        $this->assertEquals('M', $testData);
+    }
+
+    public function testItGivesMCMXCIXfor1999()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(1999);
+        $this->assertEquals('MCMXCIX', $testData);
+    }
+
+    public function testItGivesMMMMCMXCfor4990()
+    {
+        $romanNumerals = new romanNumerals;
+        $testData = $romanNumerals->convert(4990);
+        $this->assertEquals('MMMMCMXC', $testData);
+    }
+
 }
